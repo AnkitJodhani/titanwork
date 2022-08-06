7ee4699d4a281674f1f68dde61d2f2877923974f
Ankit Jodhani <64284706+AnkitJodhani@users.noreply.github.com>
2022-08-06 12:23:42 +0530

    I added bit of code for ffun. I hope you like it.

--- ankit4.py
+++ ankit4.py
@@ -3,5 +3,11 @@
 list = ["ankti","priyank","bhut","bhagat","bhadu","keyur"]
 for i in list:
     print(i)
+
+list1 = [1,2,3,4,5,6,7,8,9]
+for i in list1:
+    printf(i)
+
+
 print("ankit  is good boy ")
 print("hello world")
